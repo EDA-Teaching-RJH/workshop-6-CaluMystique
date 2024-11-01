c871847d7002c2ffd937bab380d17a12269eb10c
CaluMystique <ccal2@kent.ac.uk>
2024-11-01 15:56:02 +0000

    idk

--- starterFile.py
+++ starterFile.py
@@ -1,17 +1,38 @@
 #Your code goes here. 
 
 import math
+def main():
+    a = input("first number")
+    b = input("second number")
 
 def safe_divide(a, b):
-
+    try:
+        print(a / b)
+    except ZeroDivisionError:
+        print("Cannot divide by zero")
 
 def process_list(input_list):
-
+        for x in range(len(input_list)):
+            try:
+                input_list[x] = input_list[x] * 3
+            except TypeError and ValueError:
+                print("invalid input")
 
 def nested_operations(a, b):
+    try: 
+        print(a / b)
+        try: 
+            print(sqrt(a/b)) 
+            return "whoKnowsWhat"
+        except ZeroDivisionError: 
+            return "Its Very Broken"
+    except ZeroDivisionError: 
+        return "Its Broken"
 
 
 def process_input():
+
+
 
 
 main()
